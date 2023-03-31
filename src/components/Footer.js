efb3c2fb85c30fa1e5a8ee948094a611d04e537c
Amandeep <c0850554@mylambton.ca>
2023-03-31 17:43:20 -0400

    Completed

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="text-lg text-gray-600 font-semibold h-[56px] mt-5 flex items-center justify-center gap-x-3">
+      <p>
+      
+        
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
